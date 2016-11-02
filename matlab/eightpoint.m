77e14425fde6ae249dbe01cf556af948b27a7667
Jerry Hsiung <shsiung@g.hmc.edu>
2016-11-02 02:25:14 -0400

    working on 7-point

--- matlab/eightpoint.m
+++ matlab/eightpoint.m
@@ -11,21 +11,35 @@ function [ F ] = eightpoint( pts1, pts2, M )
 
 %     Write F and display the output of displayEpipolarF in your writeup
 
-n_pts1 = [pts1/M ones(size(pts1,1),1)];
-n_pts1 = repelem(n_pts1,1,3);
+% Normalization factor T
+T=[2/M 0  -1;
+   0 2/M  -1;
+   0   0  1];
 
-n_pts2 = [pts2/M ones(size(pts2,1),1)];
-n_pts2 = repmat(n_pts2,1,3);
+n_pts1 = [pts1 ones(size(pts2,1),1)]';
+n_pts1 = T*n_pts1;
+n_pts1 = repmat(n_pts1',1,3);
 
+n_pts2 = [pts2 ones(size(pts1,1),1)]';
+n_pts2 = T*n_pts2;
+n_pts2 = repelem(n_pts2',1,3);
+
+% Construct A matrix
 A = n_pts1.*n_pts2;
 
+% Solve for F
 [~,~,V] = svd(A);
-F = V(:,end)*M;
-F = reshape(F,3,3);
+F = V(:,end);
+F = reshape(F,3,3)';
+
+% Singularity constraint
 [UF, WF, VF] = svd(F);
 WF(3,3) = 0;
 F = UF * WF * VF';
+
+% Refine answer
 F = refineF(F,pts1,pts2);
 
+% unnormalized
+F = T' * F * T;
 end
-
--- matlab/sevenpoint.m
+++ matlab/sevenpoint.m
@@ -11,5 +11,41 @@ function [ F ] = sevenpoint( pts1, pts2, M )
 
 %     Write recovered F and display the output of displayEpipolarF in your writeup
 
-end
+T=[2/M 0  -1;
+   0 2/M  -1;
+   0   0  1];
 
+n_pts1 = [pts1 ones(size(pts2,1),1)]';
+n_pts1 = T*n_pts1;
+n_pts1 = repmat(n_pts1',1,3);
+
+n_pts2 = [pts2 ones(size(pts1,1),1)]';
+n_pts2 = T*n_pts2;
+n_pts2 = repelem(n_pts2',1,3);
+
+% Construct A matrix
+A = n_pts1.*n_pts2;
+
+% Solve for F
+[~,S,V] = svd(A);
+F1 = reshape(V(:,end),3,3)';
+F2 = reshape(V(:,end-1),3,3)';
+syms a;
+F = a*F1+(1-a)*F2;
+F = solve(det(F) == 0, a);
+
+% pick alpha such that det(alpha F1 + (1-alpha) F2) = 0
+
+% F = reshape(V(:,end),3,3)';
+% 
+% % Singularity constraint
+% [UF, WF, VF] = svd(F);
+% WF(3,3) = 0;
+% F = UF * WF * VF';
+% 
+% % Refine answer
+% F = refineF(F,pts1,pts2);
+% 
+% % unnormalized
+% F = T' * F * T;
+end
