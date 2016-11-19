686b9d72582ff1bf61c2f99e9084f40ac58a7ac9
Jerry Hsiung <shsiung@g.hmc.edu>
2016-11-19 11:42:12 -0500

    HW4 done

--- matlab/ransacF.m
+++ matlab/ransacF.m
@@ -12,8 +12,36 @@ function [ F ] = ransacF( pts1, pts2, M )
 
 %     In your writeup, describe your algorith, how you determined which
 %     points are inliers, and any other optimizations you made
+iter_max = 100;
+iter = 0;
+err_thresh = 0.002;
+num_inliers = 0;
 
+while iter < iter_max    
+    rand_pts = randsample(length(pts1),7);
+    pt1 = pts1(rand_pts,:);
+    pt2 = pts2(rand_pts,:);
+    
+    curr_F = sevenpoint( pt1, pt2, M );
+    
+    for i = 1 : length(curr_F)
+        mat_curr_F = cell2mat(curr_F(i));
+        err =  diag([pts1 ones(length(pts1),1)]...
+                     *mat_curr_F...
+                     *[pts2 ones(length(pts2),1)]');
+        inlier = abs(err)<err_thresh;
+        inline_pt1 = pts1(inlier,:);
+        inline_pt2 = pts2(inlier,:);
+        
+        if (size(inline_pt1,1) > 1)
+            mat_curr_F = eightpoint(inline_pt1,inline_pt2,M);
+            refine_err =  diag([pts1 ones(length(pts1),1)]*mat_curr_F*[pts2 ones(length(pts2),1)]');
+            num = sum(abs(refine_err)<err_thresh);
 
-
+            if num > num_inliers
+                F = mat_curr_F;
+            end
+        end
+    end
+    iter = iter + 1;
 end
-
--- matlab/sevenpoint.m
+++ matlab/sevenpoint.m
@@ -1,3 +1,64 @@
+% function [ F ] = sevenpoint( pts1, pts2, M )
+% % sevenpoint:
+% %   pts1 - Nx2 matrix of (x,y) coordinates
+% %   pts2 - Nx2 matrix of (x,y) coordinates
+% %   M    - max (imwidth, imheight)
+% 
+% % Q2.2 - Todo:
+% %     Implement the sevenpoint algorithm
+% %     Generate a matrix F from some '../data/some_corresp.mat'
+% %     Save recovered F (either 1 or 3 in cell), M, pts1, pts2 to q2_2.mat
+% 
+% %     Write recovered F and display the output of displayEpipolarF in your writeup
+% 
+% % Normalization factor T
+% T=[1/M 0  0;
+%    0 1/M  0;
+%    0   0  1];
+% 
+% n_pts1 = [pts1 ones(size(pts1,1),1)]';
+% n_pts1 = T*n_pts1;
+% n_pts1 = repmat(n_pts1',1,3);
+% 
+% n_pts2 = [pts2 ones(size(pts2,1),1)]';
+% n_pts2 = T*n_pts2;
+% n_pts2 = repelem(n_pts2',1,3);
+% 
+% % Construct A matrix
+% A = n_pts1.*n_pts2;
+% % Find null space of A
+% nullA = null(A);
+% F1 = reshape(nullA(:,1),3,3)';
+% F2 = reshape(nullA(:,2),3,3)';
+% 
+% % Solve for alpha
+% syms a;
+% F = (a)*F1+(1-a)*F2;
+% sym2poly(det(F));
+% p = roots(sym2poly(det(F)));
+% 
+% % Check for imaginary numbers
+% if (isreal(p(1)) && isreal(p(2)) && isreal(p(3)))
+%     F11 = T'*((p(1))*F1+(1-p(1))*F2)*T;
+%     F11 = F11/norm(F11);
+%     
+%     F22 = T'*((p(2))*F1+(1-p(2))*F2)*T;
+%     F22 = F22/norm(F22);
+%     
+%     F33 = T'*((p(3))*F1+(1-p(3))*F2)*T;
+%     F33 = F33/norm(F33);
+%     F = [mat2cell(F11,3,3),mat2cell(F22,3,3),mat2cell(F33,3,3)];
+% else
+%     realpart = p == real(p);
+%     F = T'*((p(realpart))*F1+(1-p(realpart))*F2)*T;
+%     F = F/norm(F);
+%     F =  mat2cell(F,3,3);
+% end
+% 
+% %save('q2_2.mat','F','M','pts1','pts2')
+% 
+% end
+
 function [ F ] = sevenpoint( pts1, pts2, M )
 % sevenpoint:
 %   pts1 - Nx2 matrix of (x,y) coordinates
@@ -5,55 +66,40 @@ function [ F ] = sevenpoint( pts1, pts2, M )
 %   M    - max (imwidth, imheight)
 
 % Q2.2 - Todo:
-%     Implement the sevenpoint algorithm
+%     Implement the eightpoint algorithm
 %     Generate a matrix F from some '../data/some_corresp.mat'
 %     Save recovered F (either 1 or 3 in cell), M, pts1, pts2 to q2_2.mat
 
 %     Write recovered F and display the output of displayEpipolarF in your writeup
 
-% Normalization factor T
-T=[1/M 0  0;
-   0 1/M  0;
-   0   0  1];
+pts1 = pts1/M;
+pts2 = pts2/M;
+N = size(pts1,1);
+T = eye(3)*1/M;
+T(3,3) = 1;
 
-n_pts1 = [pts1 ones(size(pts1,1),1)]';
-n_pts1 = T*n_pts1;
-n_pts1 = repmat(n_pts1',1,3);
+tmp = [pts1 ones(N,1)];
+A = [bsxfun(@times, pts2(:,1), tmp) bsxfun(@times, pts2(:,2), tmp) tmp];
+[~,~,V] = svd(A);
 
-n_pts2 = [pts2 ones(size(pts2,1),1)]';
-n_pts2 = T*n_pts2;
-n_pts2 = repelem(n_pts2',1,3);
+F1 = reshape(V(:,8),3,3)';
+F2 = reshape(V(:,9),3,3)';
 
-% Construct A matrix
-A = n_pts1.*n_pts2;
-% Find null space of A
-nullA = null(A);
-F1 = reshape(nullA(:,1),3,3)';
-F2 = reshape(nullA(:,2),3,3)';
-
-% Solve for alpha
 syms a;
-F = (a)*F1+(1-a)*F2;
-sym2poly(det(F));
-p = roots(sym2poly(det(F)));
+rs = double(solve(det(0 == a*F1 + (1-a)*F2) ,a));
 
-% Check for imaginary numbers
-if (isreal(p(1)) && isreal(p(2)) && isreal(p(3)))
-    F11 = T'*((p(1))*F1+(1-p(1))*F2)*T;
-    F11 = F11/norm(F11);
-    
-    F22 = T'*((p(2))*F1+(1-p(2))*F2)*T;
-    F22 = F22/norm(F22);
-    
-    F33 = T'*((p(3))*F1+(1-p(3))*F2)*T;
-    F33 = F33/norm(F33);
-    F = [mat2cell(F11,3,3),mat2cell(F22,3,3),mat2cell(F33,3,3)];
-else
-    realpart = p == real(p);
-    F = T'*((p(realpart))*F1+(1-p(realpart))*F2)*T;
-    F = F/norm(F);
-    F =  mat2cell(F,3,3);
+for i =1:3
+   if abs(imag(rs(i))) < 0.0001 %isreal(rs(i)) == 1
+       F{i} = real(F1*rs(i) + F2*(1-rs(i)));
+       F{i} = T'*F{i}*T;
+       
+      % [U S V] = svd(F{i});
+      %  S(3,3) = 0;
+      %  F{i} = U*S*V';
+       
+   end
 end
+
 
 %save('q2_2.mat','F','M','pts1','pts2')
 
